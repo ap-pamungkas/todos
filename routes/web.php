7cdef0d9dc458a28a8d2e208b7eb1bbc5bd3702d
ap-pamungkas <agustinusputra94@gmail.com>
2024-08-29 12:09:44 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index'])->name('todos.index');
 Route::post('/store', [TodoController::class, 'store'])->name('todos.store');
-Route::post('/update-status/{id}', [TodoController::class, 'updateStatus'])->name('todos.updateStatus');
+Route::put('/update-status/{id}', [TodoController::class, 'updateStatus'])->name('todos.updateStatus');
 Route::post('/clear-completed', [TodoController::class, 'clearCompleted'])->name('todos.clearCompleted');
 Route::get('/filter', [TodoController::class, 'filter'])->name('todos.filter');
 Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.destroy');
